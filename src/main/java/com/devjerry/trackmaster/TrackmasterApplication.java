aef9e41ee2a90d1617e2f61d0f0e3c042b645cb9
Jerry Sean Huynh <sjhuynh500@gmail.com>
2024-04-25 01:32:12 -0500

    first commit

---
+++ src/main/java/com/devjerry/trackmaster/TrackmasterApplication.java
@@ -0,0 +1,13 @@
+package com.devjerry.trackmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackmasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackmasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devjerry/trackmaster/TrackmasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devjerry.trackmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackmasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
